3d6d18f258205e120e234276efef29a67b9fec35
Iskhak <isakask12242001@gmail.com>
2022-12-04 01:45:42 +0300

    Facade pattern wellDone!

---
+++ src/main/java/com/iskhak/behavioral/facade/Job.java
@@ -0,0 +1,7 @@
+package com.iskhak.behavioral.facade;
+
+public class Job {
+    public void doJob(){
+        System.out.println("Job in progress...");
+    }
+}
---
+++ src/main/java/com/iskhak/behavioral/facade/SprintRunner.java
@@ -0,0 +1,8 @@
+package com.iskhak.behavioral.facade;
+
+public class SprintRunner {
+    public static void main(String[] args) {
+       WorkFlow workFlow = new WorkFlow();
+       workFlow.solveProblems();
+    }
+}
