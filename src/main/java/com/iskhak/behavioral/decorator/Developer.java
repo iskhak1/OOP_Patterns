c7f4c7a3ce35ba2ed499eb387f5ccc72508c575e
Iskhak <isakask12242001@gmail.com>
2022-12-04 01:25:12 +0300

    Decorator pattern wellDone!

---
+++ src/main/java/com/iskhak/behavioral/decorator/Developer.java
@@ -0,0 +1,5 @@
+package com.iskhak.behavioral.decorator;
+
+public interface Developer {
+    String makeJob();
+}
---
+++ src/main/java/com/iskhak/behavioral/decorator/JavaDeveloper.java
@@ -0,0 +1,8 @@
+package com.iskhak.behavioral.decorator;
+
+public class JavaDeveloper implements Developer{
+    @Override
+    public String makeJob() {
+        return "Write java code";
+    }
+}
