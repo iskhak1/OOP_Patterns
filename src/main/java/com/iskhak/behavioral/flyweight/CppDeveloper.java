6cf7f252ef17a7e7abe5985763de7fbff817efb2
Iskhak <isakask12242001@gmail.com>
2022-12-04 02:12:41 +0300

    FlyWeight pattern wellDone!

---
+++ src/main/java/com/iskhak/behavioral/flyweight/CppDeveloper.java
@@ -0,0 +1,8 @@
+package com.iskhak.behavioral.flyweight;
+
+public class CppDeveloper implements Developer{
+    @Override
+    public void writeCode() {
+        System.out.println("c++ developer writes c++ code...");
+    }
+}
---
+++ src/main/java/com/iskhak/behavioral/flyweight/Developer.java
@@ -0,0 +1,5 @@
+package com.iskhak.behavioral.flyweight;
+
+public interface Developer {
+    void writeCode();
+}
